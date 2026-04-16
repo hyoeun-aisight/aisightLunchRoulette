d4c0ce07ab3de1512170e98808bd9f19aa8ebae5
hyoeun-aisight <hyoeun.jeong@tdk.com>
2026-04-16 09:01:10 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
